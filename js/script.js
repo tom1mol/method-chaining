d975e18670597beb3c26914cd71ea5c362cbee51
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-07-11 20:09:36 +0000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ 
+
+
+
+$("button").mouseenter(function(){
+ $(this).removeClass("makeRed").addClass("makeBorder");
+});
+$("button").mouseleave(function(){
+ $("button").removeClass("makeBorder").addClass("makeRed");
+});
+}); 
